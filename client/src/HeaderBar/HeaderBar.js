1ba170e935aa4d1dc0d6ae3468bbc64840842ece
Derek <dtchiong@gmail.com>
2018-11-10 20:32:50 -0800

    Added toggle button to header bar to make it responsive on mobile

--- client/src/HeaderBar/HeaderBar.js
+++ client/src/HeaderBar/HeaderBar.js
@@ -3,53 +3,56 @@ import { Nav, Navbar, NavItem, Button } from "react-bootstrap";
 import LoginControl from "../Auth/LoginControl";
 
 class HeaderBar extends Component {
-
   render() {
-
     const navBarStyle = {
       display: "inline-block",
       float: "none",
       padding: "10px"
     };
 
-    const brandTextStyle ={
+    const brandTextStyle = {
       verticalAlign: "middle",
       fontSize: "1.5em",
       textAlign: "bottom"
-    }
+    };
 
     const navLinkStyle = {
       fontSize: "1.1em"
-    }
+    };
 
     return (
       <div className="header">
         <Navbar style={navBarStyle} inverse fixedTop>
           <Navbar.Header>
-            <Navbar.Brand style={brandTextStyle} id="brand">JobLogger</Navbar.Brand>
+            <Navbar.Brand style={brandTextStyle} id="brand">
+              JobLogger
+            </Navbar.Brand>
+            <Navbar.Toggle />
           </Navbar.Header>
-          <Nav className="tab-link" style={navLinkStyle}>
-            <NavItem eventKey={1} href="#">
-              Home
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              Profile
-            </NavItem>
-            <NavItem eventKey={3} href="#">
-              About
-            </NavItem>
-          </Nav>
-          <Nav style={navBarStyle} pullRight>
-            <LoginControl
-              user={this.props.user}
-              onRef={ref => (this.child = ref)}
-              history={this.props.history}
-              auth={this.props.auth}
-              setUserProfile={this.props.setUserProfile}
-              clearUserId={this.props.clearUserId}
-              insertUserIfNew={this.props.insertUserIfNew}
-            />
-          </Nav>
+          <Navbar.Collapse>
+            <Nav className="tab-link" style={navLinkStyle}>
+              <NavItem eventKey={1} href="#">
+                Home
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Profile
+              </NavItem>
+              <NavItem eventKey={3} href="#">
+                About
+              </NavItem>
+            </Nav>
+            <Nav style={navBarStyle} pullRight>
+              <LoginControl
+                user={this.props.user}
+                onRef={ref => (this.child = ref)}
+                history={this.props.history}
+                auth={this.props.auth}
+                setUserProfile={this.props.setUserProfile}
+                clearUserId={this.props.clearUserId}
+                insertUserIfNew={this.props.insertUserIfNew}
+              />
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
       </div>
     );

91787df82df0288f9a068209c5800b5a369093c8
Derek Chiong <dtchiong@gmail.com>
2019-01-10 23:00:39 -0800

    Added header to the profile page and made it responsive for mobile devices. Partically implemented the About page

--- client/src/Components/About/About.js
+++ client/src/Components/About/About.js
@@ -1,10 +1,59 @@
 import React, { Component } from "react";
+import { Row, Col, Panel } from "react-bootstrap";
 
 class About extends Component {
   render() {
     return (
       <div>
         <h1>About</h1>
+        <Panel>
+          <Row>
+            <Col xs={10} xsOffset={1} sm={10} smOffset={1} md={8} mdOffset={2}>
+              <Panel.Body>
+                <Panel.Title>Functionality</Panel.Title>
+                <p>
+                  This website is built with responsive design and allows users
+                  to keep track of the jobs they apply to by entering it in the
+                  list that can be saved to their user account. This list can be
+                  accessed on any device with a browser.
+                </p>
+                <Panel.Title>Motivation</Panel.Title>
+                <p>
+                  The main goal in building this website was for me to improve
+                  my web development skills and learn React. Although I had
+                  developed a web application in a group project prior to this
+                  one, that project was much larger in terms of scale, so most
+                  of us had a more focused goal and didn't have that much time
+                  to meddle with other parts of the application. For me, that
+                  meant that I was mainly developing a page for one of the
+                  site's many features, but I didn't understand well how the
+                  backend worked, especially, the page routing and user
+                  authorization. Even after reading through the back-end code,
+                  it was hard to fully understand what every line in the code
+                  doing, especially since I was new to javascript. I believe
+                  that the only way to fully understand something new, in this
+                  case--setting up user authorization, developing the api,
+                  configuring express for the backend, and setting up routing
+                  between the pages--is to implement it yourself. So that's why
+                  I made this website. As for why I chose React, I wanted to
+                  learn a front-end framework or library, and this one seems to
+                  be the most popular one due to its use of reusable components.
+                </p>
+                <Panel.Title> Technologies Used</Panel.Title>
+                <ul>
+                  <li>React</li>
+                  <li>Bootstrap/React-Bootstrap</li>
+                  <li>Nodejs/Express</li>
+                  <li>PostgreSQL</li>
+                  <li>Auth0</li>
+                  <li>Heroku</li>
+                </ul>
+                <Panel.Title>Struggles</Panel.Title>
+                <p>There were a lot of struggles(elaborate later).</p>
+              </Panel.Body>
+            </Col>
+          </Row>
+        </Panel>
       </div>
     );
   }
--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
-import {FormGroup, ControlLabel, FormControl, Glyphicon, Button} from "react-bootstrap";
+import {
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  Glyphicon,
+  Button,
+  Panel,
+  Row,
+  Col
+} from "react-bootstrap";
+import "./profile.css";
 
 class Profile extends Component {
   constructor(props, context) {
@@ -16,117 +26,133 @@ class Profile extends Component {
 
   /* Handles the text input onChange, validates the input, and sets the state */
   handleChange(e) {
-	 const val = e.target.value;
-	 const formId = e.target.id;
+    const val = e.target.value;
+    const formId = e.target.id;
 
     switch (formId) {
       case "formFirstName":
-		  this.setState({ firstName: val });
-		  this.setState({ firstNameValidation: this.validateState(formId, val)});
+        this.setState({ firstName: val });
+        this.setState({ firstNameValidation: this.validateState(formId, val) });
         break;
       case "formLastName":
-		  this.setState({ lastName: val });
-		  this.setState({ lastNameValidation: this.validateState(formId, val)});
-		  break;
-		default:
-			console.log("unimplemented form: "+formId);
+        this.setState({ lastName: val });
+        this.setState({ lastNameValidation: this.validateState(formId, val) });
+        break;
+      default:
+        console.log("unimplemented form: " + formId);
     }
   }
 
+  /* This validates the value for the given input and returns the appropriate string */
   validateState(formId, val) {
-	switch (formId) {
-	  case "formFirstName":
-		 const firstNameLen = val.length;
-		 if (firstNameLen > 50) 
-			return "error";
-		 else if (firstNameLen > 0)
-			return "success";
-		 else 
-			return null;
-			
-	  case "formLastName":
-		 const lastNameLen = val.length;
-		 if (lastNameLen > 50)
-			return "error";
-		 else if (lastNameLen > 0)
-			return "success";
-		 else
-      return null;
-    default:
-      return null;
-	}
- }
+    switch (formId) {
+      case "formFirstName":
+        const firstNameLen = val.length;
+        if (firstNameLen > 25) return "error";
+        else if (firstNameLen > 0) return "success";
+        else return null;
 
- getValidationState(formId) {
-   switch(formId) {
-		case "formFirstName":
-			return this.state.firstNameValidation;
-		case "formLastName":
-			return this.state.lastNameValidation;
-		default:
-			return null;
-	}
- }
+      case "formLastName":
+        const lastNameLen = val.length;
+        if (lastNameLen > 25) return "error";
+        else if (lastNameLen > 0) return "success";
+        else return null;
+      default:
+        return null;
+    }
+  }
 
- getGlyphState(formId) {
-	const states = {
-		success: "ok",
-		error: "remove",
-		null: "null"
-	}
+  /* Returns the appropriate string for for the given formId for the FormControl's validation prop */
+  getValidationState(formId) {
+    switch (formId) {
+      case "formFirstName":
+        return this.state.firstNameValidation;
+      case "formLastName":
+        return this.state.lastNameValidation;
+      default:
+        return null;
+    }
+  }
 
-	switch(formId) {
-		case "formFirstName":
-			return states[this.state.firstNameValidation];
-		case "formLastName":
-			return states[this.state.lastNameValidation];
-		default:
-			return null;
-	}
- }
+  /* Returns the appropriate string for the given formId for the Glyphicon's glyph prop */
+  getGlyphState(formId) {
+    const states = {
+      success: "ok",
+      error: "remove",
+      null: "null"
+    };
 
- getFormButtonState = () => {
-	if (this.state.firstNameValidation === "error" || this.state.lastNameValidation === "error") {
-		return true;
-	}
-	return false;
- }
+    switch (formId) {
+      case "formFirstName":
+        return states[this.state.firstNameValidation];
+      case "formLastName":
+        return states[this.state.lastNameValidation];
+      default:
+        return null;
+    }
+  }
+
+  /* Disables or enables the form's submit button depending on the form's validation */
+  getFormButtonState = () => {
+    if (
+      this.state.firstNameValidation === "error" ||
+      this.state.lastNameValidation === "error"
+    ) {
+      return true;
+    }
+    return false;
+  };
 
   render() {
-    const email = (this.props.user.email? this.props.user.email : "");
+    const email = this.props.user.email ? this.props.user.email : "";
     //console.log(this.state);
     return (
       <div>
         <h1>Profile</h1>
-        <form>
-          <FormGroup
-            controlId="formFirstName"
-            validationState={this.getValidationState("formFirstName")}
-          >
-            <ControlLabel>First Name</ControlLabel>
-            <FormControl type="text" onChange={this.handleChange} />
-            <FormControl.Feedback>
-              <Glyphicon glyph={this.getGlyphState("formFirstName")}/>
-            </FormControl.Feedback>
-          </FormGroup>
+        <Panel>
+          <Row>
+            <Col xs={10} xsOffset={1} sm={10} smOffset={1} md={8} mdOffset={2}>
+              <Panel.Body>
+                <h3 id="profile-header">Edit Profile </h3>
+                <form>
+                  <FormGroup
+                    controlId="formFirstName"
+                    validationState={this.getValidationState("formFirstName")}
+                  >
+                    <ControlLabel>First Name</ControlLabel>
+                    <FormControl type="text" onChange={this.handleChange} />
+                    <FormControl.Feedback>
+                      <Glyphicon glyph={this.getGlyphState("formFirstName")} />
+                    </FormControl.Feedback>
+                  </FormGroup>
 
-          <FormGroup
-            controlId="formLastName"
-            validationState={this.getValidationState("formLastName")}
-          >
-            <ControlLabel>Last Name</ControlLabel>
-            <FormControl type="text" onChange={this.handleChange} />
-            <FormControl.Feedback>
-              <Glyphicon glyph={this.getGlyphState("formLastName")}/>
-            </FormControl.Feedback>
-          </FormGroup>
+                  <FormGroup
+                    controlId="formLastName"
+                    validationState={this.getValidationState("formLastName")}
+                  >
+                    <ControlLabel>Last Name</ControlLabel>
+                    <FormControl type="text" onChange={this.handleChange} />
+                    <FormControl.Feedback>
+                      <Glyphicon glyph={this.getGlyphState("formLastName")} />
+                    </FormControl.Feedback>
+                  </FormGroup>
 
-          <FormGroup controlId="formEmail">
-            <ControlLabel>Email Address</ControlLabel>
-            <FormControl type="text" value={email} readOnly />
-          </FormGroup>
-          <Button type="submit" disabled={this.getFormButtonState()} >Submit</Button>
-        </form>
+                  <FormGroup controlId="formEmail">
+                    <ControlLabel>Email Address</ControlLabel>
+                    <FormControl type="text" value={email} readOnly />
+                  </FormGroup>
+                  <Button
+                    bsSize="large"
+                    type="submit"
+                    disabled={this.getFormButtonState()}
+                  >
+                    Submit
+                  </Button>
+                </form>
+              </Panel.Body>
+            </Col>
+          </Row>
+        </Panel>
       </div>
     );
   }
